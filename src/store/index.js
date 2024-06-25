1f5aee4ed60b3113dce52b8fdbb3c2ea250f3dd0
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-25 14:58:06 +0200

    Add everything

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,30 @@
 import { createStore } from 'vuex'
-
+const productURL = 'http://localhost:3000/output'
 export default createStore({
   state: {
+    products: null,
+    product: null,
   },
   getters: {
   },
-  mutations: {
+  mutations: {//we update our states
+    setProducts(state, products){
+      state.products = products
+    },
+    setProduct(state, product){
+      state.product = product
+    }
   },
-  actions: {
+  actions: { //fetch, update, delete
+    async fetchProducts(context){
+      try {
+        let res = await fetch(productURL)
+        let data = await res.json()
+        context.commit('setProducts' , data)
+      } catch (e){
+        console.log(e.message);
+      }
+    }
   },
   modules: {
   }
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,58 @@
+ <template>
+    <div class="container">
+        <div class="row">
+            <h2>Products</h2>
+        </div>
+        <div class="row gap-2 justify-content-center" v-if="products?.length">
+            <Card v-for="product in products" :key="product.id">
+                <template #cardHeader>
+                    <img :src="product.ImageUrl" :alt="product.flowerName" loading="lazy" class="img-fluid">
+                </template>
+                <template #cardBody>
+                    <h5 class="card-title">{{ product.Description }}
+                    </h5>
+                    <p class="lead">{{ product.Price }}</p>
+                    <!-- <p class="shadow">R {{ product.amount }}</p> -->
+                </template>
+            </Card>
+        </div>
+        <div v-else class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Card from '@/components/Card.vue';
+export default {
+    components: {
+        Card
+    },
+    computed: {
+        products() {
+            return this.$store.state.products
+        }
+    },
+    beforeCreate(){
+       alert("Before create");
+    },
+    created(){ 
+        alert("Created")
+    },
+    beforeMount(){
+        alert("Before mount")
+    },
+    mounted() {
+        this.$store.dispatch('fetchProducts')
+    },
+    updated(){
+
+    },
+    unmounted(){
+        alert("unmounted")
+    }
+}
+</script>
+<style scoped>
+
+</style>
